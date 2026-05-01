0d0dca7317f4b7960d0a5c2e4d3d5b561489091b
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-04-30 21:08:05 -0500

    add sales by date

--- app/admin/sales/page.tsx
+++ app/admin/sales/page.tsx
@@ -1,14 +1,26 @@
 import TransactionFilter from "@/components/transactions/TransactionFilter";
 import Heading from "@/components/ui/Heading";
+import { getSalesByDate } from "@/src/api";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import { format } from "date-fns";
 
-export default function SalesPage() {
+export default async function SalesPage() {
+  const queryClient = new QueryClient()
+  const today = new Date()
+  const formatterDate = format(today, "yyyy-MM-dd");
+  await queryClient.prefetchQuery({
+    queryKey: ['sales', formatterDate],
+    queryFn: () => getSalesByDate(formatterDate)
+  })
   return (
     <>
       <Heading>Ventas</Heading>
       <p className="text-lg">
         En esta sección podrás ver las ventas, utiliza el calendario para filtrar por fecha
       </p>
-      <TransactionFilter />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <TransactionFilter />
+      </HydrationBoundary>
     </>
   )
 }
